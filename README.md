f859f1d79911061668ce11075b568fadff68859b
dominic <dominic@domm.me>
2023-01-18 15:47:20 +0000

    link shortener!:D

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # link-shortener
-A link shortener for Cloudflare workers
+
+link-shortener is a simple URL shortener for Cloudflare workers!
+
+## Installation
+
+To start, clone this repository and after:
+
+```sh
+# Install NPM packages
+$ npm install
+# Fill out `wrangler.example.toml` with the necessary information then rename it
+$ mv wrangler.example.toml wrangler.toml
+# Add secret to worker
+$ npm run secret
+# Deploy worker
+$ npm run deploy
+```
+
+## Usage
+
+To `PUT` a new link
+
+```shell
+curl -X PUT http://127.0.0.1:8787 \
+    -H "Authorization: SECRET" \
+    -d '{"url": "URL"}'
+```
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+export interface Env {
+  KV: KVNamespace;
+  SECRET: string;
+}
+
+interface Link {
+  url: string;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    if (request.method == "PUT") {
+      return put(request, env, ctx);
+    } else if (request.method == "GET") {
+      return get(request, env, ctx);
+    }
+
+    return Response.json({ status: "Unsupported method" }, { status: 405 });
+  },
+};
+
+async function put(
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+): Promise<Response> {
+  const authorization = request.headers.get("Authorization");
+
+  if (!authorization || authorization != env.SECRET) {
+    return Response.json({ status: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await request.json<Link>();
+  const key = (Math.random() + 1).toString(36).substring(6);
+
+  await env.KV.put(key, JSON.stringify(body));
+
+  return Response.json({ key: key });
+}
+
+async function get(
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+): Promise<Response> {
+  const url = new URL(request.url);
+  const { pathname } = url;
+  const id = pathname.replace("/", "");
+
+  const link = await env.KV.get<Link>(id, { type: "json" });
+
+  if (!link) {
+    return Response.json({ status: "Unknown id" }, { status: 404 });
+  }
+
+  return Response.redirect(link.url, 301);
+}
